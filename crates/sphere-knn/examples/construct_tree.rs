57e828d7d191d7c8c270084ad650dd2e36709f0b
Nick Rakochy <nrakochy@users.noreply.github.com>
2024-02-14 20:22:41 -0600

    feat: enforce SphereKnnGetters trait in the lib entry point (#11)

--- crates/sphere-knn/examples/construct_tree.rs
+++ crates/sphere-knn/examples/construct_tree.rs
@@ -1,19 +1,19 @@
-use sphere_knn::{build_tree, Data};
+use sphere_knn::{build_tree, LocationData};
 
 fn main() {
     let data = vec![
-        Data::new(42.358, -71.064, "Boston"),
-        Data::new(42.732, -73.693, "Troy"),
-        Data::new(40.664, -73.939, "New York"),
-        Data::new(25.788, -80.224, "Miami"),
-        Data::new(51.507, -0.128, "London"),
-        Data::new(48.857, 2.351, "Paris"),
-        Data::new(48.208, 16.373, "Vienna"),
-        Data::new(41.900, 12.500, "Rome"),
-        Data::new(39.914, 116.392, "Beijing"),
-        Data::new(22.278, 114.159, "Hong Kong"),
-        Data::new(37.567, 126.978, "Seoul"),
-        Data::new(35.690, 139.692, "Tokyo"),
+        LocationData::new(42.358, -71.064, "Boston"),
+        LocationData::new(42.732, -73.693, "Troy"),
+        LocationData::new(40.664, -73.939, "New York"),
+        LocationData::new(25.788, -80.224, "Miami"),
+        LocationData::new(51.507, -0.128, "London"),
+        LocationData::new(48.857, 2.351, "Paris"),
+        LocationData::new(48.208, 16.373, "Vienna"),
+        LocationData::new(41.900, 12.500, "Rome"),
+        LocationData::new(39.914, 116.392, "Beijing"),
+        LocationData::new(22.278, 114.159, "Hong Kong"),
+        LocationData::new(37.567, 126.978, "Seoul"),
+        LocationData::new(35.690, 139.692, "Tokyo"),
     ];
     let tree = build_tree(data);
     println!("{:#?}", tree);
--- crates/sphere-knn/src/sphere_knn.rs
+++ crates/sphere-knn/src/sphere_knn.rs
@@ -1,6 +1,9 @@
 use crate::{
-    lla_node::NodeOrData, neighbors::get_nearest_neighbors, tree::build_tree,
-    utils::spherical_to_cartesian, Data, Opts,
+    lla_node::{NodeOrData, SphereKnnGetters},
+    neighbors::get_nearest_neighbors,
+    tree::build_tree,
+    utils::spherical_to_cartesian,
+    LocationData, Opts,
 };
 
 use core::fmt::Debug;
@@ -15,112 +18,241 @@ fn lookup_wrapper<T: Clone>(tree: NodeOrData<T>) -> impl Fn(f64, f64, Opts) -> V
     move |lat: f64, lng: f64, opts: Opts| return lookup(tree.clone(), lat, lng, opts)
 }
 
-pub fn sphere_knn<T: Clone + Debug>(data: Vec<Data<T>>) -> impl Fn(f64, f64, Opts) -> Vec<T> {
+fn shape_data<T: Clone + SphereKnnGetters>(data: Vec<T>) -> Vec<LocationData<T>> {
+    return data
+        .into_iter()
+        .map(|entry| {
+            let lat = entry.get_lat();
+            let lng = entry.get_lng();
+            return LocationData::new(lat, lng, entry);
+        })
+        .collect();
+}
+
+fn init<T: Clone + Debug>(data: Vec<LocationData<T>>) -> impl Fn(f64, f64, Opts) -> Vec<T> {
     let tree = build_tree(data);
     return lookup_wrapper(tree);
+}
+pub fn sphere_knn<T: Clone + SphereKnnGetters + Debug>(
+    data: Vec<T>,
+) -> impl Fn(f64, f64, Opts) -> Vec<T> {
+    let nodes = shape_data(data);
+    return init(nodes);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    impl<'name> SphereKnnGetters for TestData<'name> {
+        fn get_lat(&self) -> f64 {
+            return self.latitude;
+        }
+        fn get_lng(&self) -> f64 {
+            return self.longitude;
+        }
+    }
+
+    #[derive(Debug, Clone, Copy, PartialEq)]
+    struct TestData<'name> {
+        latitude: f64,
+        longitude: f64,
+        name: &'name str,
+    }
+    const TOKYO: TestData<'_> = TestData {
+        latitude: 35.690,
+        longitude: 139.692,
+        name: "Tokyo",
+    };
+    const BOSTON: TestData<'_> = TestData {
+        latitude: 42.358,
+        longitude: -71.064,
+        name: "Boston",
+    };
+    const TROY: TestData<'_> = TestData {
+        latitude: 42.732,
+        longitude: -73.693,
+        name: "Troy",
+    };
+    const NY: TestData<'_> = TestData {
+        latitude: 40.664,
+        longitude: -73.939,
+        name: "New York",
+    };
+    const MIAMI: TestData<'_> = TestData {
+        latitude: 25.788,
+        longitude: -80.224,
+        name: "Miami",
+    };
+    const LONDON: TestData<'_> = TestData {
+        latitude: 51.507,
+        longitude: -0.128,
+        name: "London",
+    };
+    const PARIS: TestData<'_> = TestData {
+        latitude: 48.857,
+        longitude: 2.351,
+        name: "Paris",
+    };
+    const VIENNA: TestData<'_> = TestData {
+        latitude: 48.208,
+        longitude: 16.373,
+        name: "Vienna",
+    };
+    const ROME: TestData<'_> = TestData {
+        latitude: 41.900,
+        longitude: 12.500,
+        name: "Rome",
+    };
+    const BEIJING: TestData<'_> = TestData {
+        latitude: 39.914,
+        longitude: 116.392,
+        name: "Beijing",
+    };
+    const HONG_KONG: TestData<'_> = TestData {
+        latitude: 22.278,
+        longitude: 114.159,
+        name: "Hong Kong",
+    };
+    const SEOUL: TestData<'_> = TestData {
+        latitude: 37.567,
+        longitude: 126.978,
+        name: "Seoul",
+    };
+    const LOS_ANGELES: TestData<'_> = TestData {
+        latitude: 34.0549,
+        longitude: -118.2426,
+        name: "Los Angeles",
+    };
+    const MEXICO_CITY: TestData<'_> = TestData {
+        latitude: 19.4326,
+        longitude: -99.1332,
+        name: "Mexico City",
+    };
+
     #[test]
     fn test_one_result() {
-        let ny = Data::new(40.664, -73.939, "New York");
         let data = vec![
-            Data::new(35.690, 139.692, "Tokyo"),
-            Data::new(42.358, -71.064, "Boston"),
-            Data::new(42.732, -73.693, "Troy"),
-            ny,
-            Data::new(25.788, -80.224, "Miami"),
-            Data::new(51.507, -0.128, "London"),
-            Data::new(48.857, 2.351, "Paris"),
-            Data::new(48.208, 16.373, "Vienna"),
-            Data::new(41.900, 12.500, "Rome"),
-            Data::new(39.914, 116.392, "Beijing"),
-            Data::new(22.278, 114.159, "Hong Kong"),
-            Data::new(37.567, 126.978, "Seoul"),
-            Data::new(35.690, 139.692, "Tokyo"),
+            TOKYO,
+            BOSTON,
+            TROY,
+            NY,
+            MIAMI,
+            LONDON,
+            PARIS,
+            VIENNA,
+            ROME,
+            BEIJING,
+            HONG_KONG,
+            SEOUL,
+            LOS_ANGELES,
+            MEXICO_CITY,
         ];
         let find_nearest = sphere_knn(data);
-        let philly = Data::new(39.95, -75.17, "Philadelphia");
+        let philly = TestData {
+            latitude: 39.95,
+            longitude: -75.17,
+            name: "Philadelphia",
+        };
         let result = find_nearest(
-            philly.lat,
-            philly.lng,
+            philly.get_lat(),
+            philly.get_lng(),
             Opts {
                 max_distance_threshold_meters: None,
                 number_results: Some(1 as usize),
             },
         );
-        assert_eq!(result, vec![ny.data]);
+        assert_eq!(result, vec![NY]);
     }
 
     #[test]
     fn test_four_results_correct_order_multiple_cities() {
         let data = vec![
-            Data::new(35.690, 139.692, "Tokyo"),
-            Data::new(42.358, -71.064, "Boston"),
-            Data::new(42.732, -73.693, "Troy"),
-            Data::new(40.664, -73.939, "New York"),
-            Data::new(25.788, -80.224, "Miami"),
-            Data::new(51.507, -0.128, "London"),
-            Data::new(48.857, 2.351, "Paris"),
-            Data::new(48.208, 16.373, "Vienna"),
-            Data::new(41.900, 12.500, "Rome"),
-            Data::new(39.914, 116.392, "Beijing"),
-            Data::new(22.278, 114.159, "Hong Kong"),
-            Data::new(37.567, 126.978, "Seoul"),
-            Data::new(34.0549, -118.2426, "Los Angeles"),
-            Data::new(19.4326, -99.1332, "Mexico City"),
+            TOKYO,
+            BOSTON,
+            TROY,
+            NY,
+            MIAMI,
+            LONDON,
+            PARIS,
+            VIENNA,
+            ROME,
+            BEIJING,
+            HONG_KONG,
+            SEOUL,
+            LOS_ANGELES,
+            MEXICO_CITY,
         ];
         let find_nearest = sphere_knn(data);
         let opts = Opts {
             max_distance_threshold_meters: None,
             number_results: Some(4 as usize),
         };
-        let philly = Data::new(39.95, -75.17, "Philadelphia");
-        let result1 = find_nearest(philly.lat, philly.lng, opts);
+        let philly = TestData {
+            latitude: 39.95,
+            longitude: -75.17,
+            name: "Philadelphia",
+        };
+        let result1 = find_nearest(philly.get_lat(), philly.get_lng(), opts);
         //       [vienna, paris, london, rome]
-        let berlin = Data::new(52.50, 13.40, "Berlin");
-        let result2 = find_nearest(berlin.lat, berlin.lng, opts);
+        let berlin = TestData {
+            latitude: 52.50,
+            longitude: 13.40,
+            name: "Berlin",
+        };
+        let result2 = find_nearest(berlin.get_lat(), berlin.get_lng(), opts);
 
-        let hawaii = Data::new(21.31, -157.80, "Hawaii");
-        let result3 = find_nearest(hawaii.lat, hawaii.lng, opts);
-        assert_eq!(result1, vec!["New York", "Troy", "Boston", "Miami"]);
-        assert_eq!(result2, vec!["Vienna", "Paris", "London", "Rome"]);
+        let hawaii = TestData {
+            latitude: 21.31,
+            longitude: -157.80,
+            name: "Hawaii",
+        };
+        let result3 = find_nearest(hawaii.get_lat(), hawaii.get_lng(), opts);
+        assert_eq!(result1, vec![NY, TROY, BOSTON, MIAMI]);
+        assert_eq!(result2, vec![VIENNA, PARIS, LONDON, ROME]);
         assert_eq!(
             result3,
-            vec!["Los Angeles", "Mexico City", "Tokyo", "Seoul"]
+            vec![
+                LOS_ANGELES.clone(),
+                MEXICO_CITY.clone(),
+                TOKYO.clone(),
+                SEOUL.clone()
+            ]
         );
     }
 
     #[test]
     fn test_respects_max_distance() {
         let data = vec![
-            Data::new(35.690, 139.692, "Tokyo"),
-            Data::new(42.358, -71.064, "Boston"),
-            Data::new(42.732, -73.693, "Troy"),
-            Data::new(40.664, -73.939, "New York"),
-            Data::new(25.788, -80.224, "Miami"),
-            Data::new(51.507, -0.128, "London"),
-            Data::new(48.857, 2.351, "Paris"),
-            Data::new(48.208, 16.373, "Vienna"),
-            Data::new(41.900, 12.500, "Rome"),
-            Data::new(39.914, 116.392, "Beijing"),
-            Data::new(22.278, 114.159, "Hong Kong"),
-            Data::new(37.567, 126.978, "Seoul"),
-            Data::new(35.690, 139.692, "Tokyo"),
+            TOKYO,
+            BOSTON,
+            TROY,
+            NY,
+            MIAMI,
+            LONDON,
+            PARIS,
+            VIENNA,
+            ROME,
+            BEIJING,
+            HONG_KONG,
+            SEOUL,
+            LOS_ANGELES,
+            MEXICO_CITY,
         ];
         let find_nearest = sphere_knn(data);
-        let hartford = Data::new(41.76, -72.67, "hartford");
+        let hartford = TestData {
+            latitude: 41.76,
+            longitude: -72.67,
+            name: "hartford",
+        };
         let result = find_nearest(
-            hartford.lat,
-            hartford.lng,
+            hartford.get_lat(),
+            hartford.get_lng(),
             Opts {
                 max_distance_threshold_meters: Some(200_000.0),
                 number_results: Some(20 as usize),
             },
         );
-        assert_eq!(result, vec!["Troy", "Boston", "New York"]);
+        assert_eq!(result, vec![TROY, BOSTON, NY]);
     }
 }
